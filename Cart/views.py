51f43e48ec0b699ae1659cd1e6586b667928f611
Ayush-kumar <ayush.kumaer.cse22@itbhu.ac.in>
2023-11-03 18:32:12 +0530

    Completed Login signup logout and linked to home as well

--- Cart/views.py
+++ Cart/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def cart(request):
+    if not User.is_authenticated :
+        return HttpResponseRedirect('/user/login')
+    return render(request,'cart.html')
+
